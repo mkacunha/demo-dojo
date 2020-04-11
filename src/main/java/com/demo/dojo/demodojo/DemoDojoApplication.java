e9d38298676da88f11ab852c08311bedb9162067
Maiko Cunha <maiko.cunha@db1.com.br>
2020-04-11 15:57:16 -0300

    .

---
+++ src/main/java/com/demo/dojo/demodojo/DemoDojoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.dojo.demodojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDojoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/dojo/demodojo/DemoDojoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.dojo.demodojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDojoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
